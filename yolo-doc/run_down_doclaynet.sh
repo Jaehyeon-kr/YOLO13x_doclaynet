d11b2dfa1fc7b2b328f86936d643ed0fef6337aa
Jaehyeon-kr <qmdlghfl3@naver.com>
2025-10-02 16:54:19 +0900

    Update run_down_doclaynet.sh

--- yolo-doc/run_down_doclaynet.sh
+++ yolo-doc/run_down_doclaynet.sh
@@ -3,13 +3,13 @@
 # 1. DocLayNet 데이터셋 다운로드
 curl -L -o DocLayNet_core.zip https://codait-cos-dax.s3.us.cloud-object-storage.appdomain.cloud/dax-doclaynet/1.0.0/DocLayNet_core.zip
 
-# # 2. datasets 디렉토리 생성
+# 2. datasets 디렉토리 생성
 mkdir -p datasets_doc
 
-# # 3. 다운로드한 zip 파일 이동
+# 3. 다운로드한 zip 파일 이동
 mv DocLayNet_core.zip datasets_doc/
 
-# # 4. 압축 해제 후 zip 파일 삭제
+# 4. 압축 해제 후 zip 파일 삭제
 cd datasets_doc/ && unzip DocLayNet_core.zip && rm DocLayNet_core.zip
 cd ..
 
@@ -17,5 +17,6 @@ cd ..
 python convert_dataset_doc.py
 
 
-chmod +x run_doclaynet.sh   # 실행 권한 부여
-./run_doclaynet.sh          # 스크립트 실행(Git bash)
+chmod +x run_doclaynet.sh   
+./run_doclaynet.sh       
+
